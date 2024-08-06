b1068dd4a1fa36f73bcead734c197225dfe181d4
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 10:34:05 +0530

    LeapYear:commit7:Refactoring code

--- src/main/java/com/example/LeapYear.java
+++ src/main/java/com/example/LeapYear.java
@@ -2,14 +2,6 @@ package com.example;
 
 public class LeapYear {
     public boolean isLeap(int year){
-        boolean ans;
-        if(year%400==0){
-            ans=true;
-        }else if(year%4==0&& year%100!=0){
-            ans=true;
-        }else{
-            ans=false;
-        }
-        return ans;
+        return year%4==0 &&(year%400==0||year%100!=0);
     }
 }
