8bd2852bf4211cddb0b0eb0cb851fc9fdd3a70a5
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 17:34:41 +0530

    lcp:commit2:passing tests

---
+++ src/main/java/com/example/longestCommonPrefix.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class longestCommonPrefix {
+    public static String lCommonPrefix(String[] strs){
+        if(strs==null || strs.length==0){
+            return "";
+        }
+    }
+}
