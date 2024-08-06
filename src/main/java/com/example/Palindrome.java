333c7341be1a86ebd1cf6895cfe202aecd7e1ebc
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 10:16:45 +0530

    Palindrome:commit4:Passing tests

--- src/main/java/com/example/Palindrome.java
+++ src/main/java/com/example/Palindrome.java
@@ -2,6 +2,15 @@ package com.example;
 
 public class Palindrome {
     public boolean isPalindrome(String str){
+        int right=str.length()-1;
+        int left=0;
+        while(right>left){
+            if(str.charAt(right)!=str.charAt(left)){
+                return false;
+            }
+            right--;
+            left++;
+        }
         return true;
     }
 }
