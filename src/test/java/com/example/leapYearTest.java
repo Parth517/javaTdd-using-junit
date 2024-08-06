2788ee2dde8f8f147e85b636da3881ee56cb0d06
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 10:21:51 +0530

    LeapYear:commit1:initial commit

---
+++ src/test/java/com/example/leapYearTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class leapYearTest {
+    leapYear helper=new leapYear();
+
+    @Test
+    public void testLeapYear(){
+        
+    }
+}
