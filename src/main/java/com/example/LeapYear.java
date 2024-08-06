c405caf3c5efd8dd1e5967b72ab6c458c5fc4f60
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 10:31:38 +0530

    LeapYear:commit6:Passing testcases

--- src/main/java/com/example/LeapYear.java
+++ src/main/java/com/example/LeapYear.java
@@ -1,7 +1,15 @@
 package com.example;
 
 public class LeapYear {
-    public boolean isLeap(int num){
-        return true;
+    public boolean isLeap(int year){
+        boolean ans;
+        if(year%400==0){
+            ans=true;
+        }else if(year%4==0&& year%100!=0){
+            ans=true;
+        }else{
+            ans=false;
+        }
+        return ans;
     }
 }
