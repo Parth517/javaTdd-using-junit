450cd4ab35c328bf3cbe768c2a4d2987e88259e6
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 10:03:18 +0530

    Palinfrome:commit2:Write test cases

--- src/test/java/com/example/palindromeTest.java
+++ src/test/java/com/example/palindromeTest.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import org.junit.Test;
+
 public class palindromeTest {
     Palindrome helper=new Palindrome();
+    @Test
+    publi void testPalindrome(){
+        assetEquals("abcba",helper.isPalindrome(true));
+        assetEquals("abcdba",helper.isPalindrome(false));
+        assetEquals("wow",helper.isPalindrome(true));
+        assetEquals("abcdcba",helper.isPalindrome(true));
+    }
 }
